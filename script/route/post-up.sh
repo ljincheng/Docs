4949286241481888a56769f178861a8800c4abf6
linjincmf <linjincmf@fansfinancial.com>
2021-01-13 16:47:04 +0800

    完善

--- script/route/post-up.sh
+++ script/route/post-up.sh
@@ -2,8 +2,10 @@
 
 os=`uname -s`
 
-ips16_group1=("10.50" "10.52" "172.10" "139.9")
-ips16_group2=("23.219" "14.29" "202.104" "125.94" "59.37" "121.9" "222.79" "61.177" " 125.77")
+ips16_group1=("10.50" "10.52"  "172.10" "139.9")
+ips24_group1=("192.168.212" "192.168.0" "192.168.199")
+ipsFull_group1=("127.0.0.53")
+ips16_group2=("23.219" "113.96" "119.147" "14.29" "202.104" "125.94" "59.37" "121.9" "222.79" "14.215" "61.177" " 125.77" "183.2")
 
 
 addRuleIP(){
@@ -11,25 +13,62 @@ net_gateway=$1
 net_int=$2
 ips=(` echo "$3"`)
 
-for i in ${!ips[*]}
-do
-echo "sudo route -nv add ${ips[$i]}.0.0/${net_int} ${net_gateway}"
-sudo route -nv add ${ips[$i]}.0.0/${net_int} ${net_gateway}
-done
+ips_other=".0.0/16"
+if [ ${net_int} == '24' ];then
+ips_other=".0/24"
+fi
+if [ ${net_int} == '0' ];then
+ips_other=""
+fi
+ 
+if [ ${os} == 'Darwin' ];then
+	for i in ${!ips[*]}
+	do
+	echo "route -nv add ${ips[$i]}${ips_other} ${net_gateway}"
+	sudo route -nv add ${ips[$i]}${ips_other} ${net_gateway}
+	done
+else
+	for i in ${!ips[*]}
+	do
+	echo "ip rule add to ${ips[$i]}${ips_other} priority 1024"
+	sudo ip rule add to ${ips[$i]}${ips_other} priority 1024
+	done
+fi
+
 
 }
 
 downRuleIP(){
 ips16=(` echo "$2"`)
 net_int=$1
-for i in ${!ips16[*]}
-do
-echo "sudo route delete ${ips16[$i]}.0.0/${net_int}"
-sudo route delete ${ips16[$i]}.0.0/${net_int}
-done
+
+ips_other=".0.0/16"
+if [ ${net_int} == '24' ];then
+ips_other=".0/24"
+fi
+if [ ${net_int} == '0' ];then
+ips_other=""
+fi
+
+if [ ${os} == 'Darwin' ];then
+	for i in ${!ips16[*]}
+	do
+	echo "route delete ${ips16[$i]}${ips_other}"
+	sudo route delete ${ips16[$i]}${ips_other}
+	done
+else
+	for i in ${!ips16[*]}
+	do
+	echo "ip rule delete to ${ips16[$i]}${ips_other}"
+	sudo ip rule delete to ${ips16[$i]}${ips_other}
+	done
+fi
+
 }
 
 addMyRoute(){
+
+net_gateway="10.50.41.254"
 if [ ${os} == 'Darwin' ];then
 
 ## 查看默认路由netstat -r中的default
@@ -39,24 +78,26 @@ net_gateway=`netstat -nr |grep 'default' |awk '{print $2}' |awk 'NR==2{print}'`
 
 echo "当前默认网关:${net_gateway}"
 ## net_gateway=10.50.41.254
-addRuleIP ${net_gateway} 16 "${ips16_group1[*]}"
-addRuleIP ${net_gateway} 16 "${ips16_group2[*]}"
 else
 
-#sudo ip rule add to 45.249.0.0/24 priority 1024
-#sudo ip rule add to 10.0.0.0/16 priority 1024
-echo "linux add rule"
+net_gateway=`netstat -nr |grep '0.0.0.0' | awk '{print $2}' | awk 'NR==2{print}'`
+echo "当前默认网关:${net_gateway}"
 fi
+
+addRuleIP ${net_gateway} 16 "${ips16_group1[*]}"
+addRuleIP ${net_gateway} 16 "${ips16_group2[*]}"
+addRuleIP ${net_gateway} 24 "${ips24_group1[*]}"
+addRuleIP ${net_gateway} 0 "${ipsFull_group1[*]}"
 
 }
 
 deleteMyRoute(){
-if [ ${os} == 'Darwin' ];then
+
 downRuleIP 16 "${ips16_group1[*]}"
 downRuleIP 16 "${ips16_group2[*]}"
-else
-echo "linux delete rule"
-fi
+downRuleIP 24 "${ips24_group1[*]}"
+downRuleIP 0 "${ipsFull_group1[*]}"
+
 }
 
 
